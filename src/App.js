f4b9c15c5c9d955219aa05d331f1baf9046c442d
Tejashr <tejasteju11@gmail.com>
2021-11-25 19:36:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/home";
+import Survey from "./components/survey";
+import Register from "./components/register";
+import Login from "./components/login";
+import Nav from "./components/nav";
+import { UserProvider } from "./components/usercontext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nav></Nav>
+        <div className="container">
+          <UserProvider>
+            <Switch>
+              <Route path="/login" component={Login} exact />
+              <Route path="/register" component={Register} exact />
+              <Route path="/home" component={Home} exact />
+              <Route path="/survey/:id" component={Survey} exact />
+            </Switch>
+          </UserProvider>
+        </div>
+      </Router>
+    </>
   );
 }
 
